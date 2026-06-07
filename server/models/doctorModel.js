39dd976f76025e30916b405757b72ddc1c2cef84
Your Name <utkarshjha832@gmail.com>
2026-06-07 23:21:33 +0530

    feat: implement admin authentication middleware, add login functionality, and enhance doctor address schema

--- server/models/doctorModel.js
+++ server/models/doctorModel.js
@@ -40,9 +40,15 @@ const doctorSchema = new mongoose.Schema(
       required: true,
     },
     address: {
-      type: String,
-      required: true,
-    },
+  line1: {
+    type: String,
+    required: true,
+  },
+  line2: {
+    type: String,
+    required: true,
+  },
+},
     available: {
       type: Boolean,
       default: true,
